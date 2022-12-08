d8946e51e2ab88bb84325247a247c7c5beff3637
Iris Morelle <iris@irydacea.me>
2022-12-08 20:40:45 -0300

    Made skintone modifier stripping less nonsensical
    Now we actually do just that instead of looking at all components
    of a multicharacter emoji and matching them with the bad emoji list.

---
+++ lib/strutil.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+'''
+codename Tessa
+
+Copyright (C) 2022 by Iris Morelle <iris@irydacea.me>
+See COPYING for use and distribution terms.
+'''
+
+_FITZPATRICK_MODIFIERS = (
+    '\U0001F3FB', # EMOJI MODIFIER FITZPATRICK TYPE-1-2
+    '\U0001F3FC', # EMOJI MODIFIER FITZPATRICK TYPE-3
+    '\U0001F3FD', # EMOJI MODIFIER FITZPATRICK TYPE-4
+    '\U0001F3FE', # EMOJI MODIFIER FITZPATRICK TYPE-5
+    '\U0001F3FF', # EMOJI MODIFIER FITZPATRICK TYPE-6
+)
+
+def neutral_skintone(emoji: str) -> str:
+    '''
+    Returns the specified string without Fitzpatrick skin type modifiers.
+    '''
+    return emoji.translate({ord(char): None for char in _FITZPATRICK_MODIFIERS})
